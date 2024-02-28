d92c5e857b7266f55d5720a75d5442189dcc53ef
Aziz <aziz.zina2001@gmail.com>
2024-02-28 17:39:18 +0100

    Updated the authorization for every user

--- src/main/java/com/testKeycloak/keycloak/DemoController.java
+++ src/main/java/com/testKeycloak/keycloak/DemoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @GetMapping
-    @PreAuthorize("hasAnyRole()")
+    @PreAuthorize("hasRole('client_user')")
     public String hello(){
         return "Hello World!";
     }
