9cbe443329d28718b43384c1258737f4df858462
Aziz <aziz.zina2001@gmail.com>
2024-02-29 12:15:28 +0100

    added the getClaimAsString method

--- src/main/java/com/testKeycloak/keycloak/DemoController.java
+++ src/main/java/com/testKeycloak/keycloak/DemoController.java
@@ -1,12 +1,12 @@
 package com.testKeycloak.keycloak;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.ui.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/demo")
@@ -14,8 +14,8 @@ public class DemoController {
 
     @GetMapping
     @PreAuthorize("hasAnyRole('client_admin', 'client_user')")
-    public String hello(){
-        return "Hello World!";
+    public String hello(@AuthenticationPrincipal Jwt jwt){
+        return "Hello World " + jwt.getClaimAsString("email") + " !";
     }
 
     @GetMapping("/hello-admin")
